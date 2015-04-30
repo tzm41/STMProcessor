bd932b8e47f81f56161041a08099ff5e43a5eac4
Zhuoming Tan <guangzhoutzm@gmail.com>
2015-04-29 20:36:11 -0400

    DB update

--- Database/dbcreate.py
+++ Database/dbcreate.py
@@ -1,25 +1,35 @@
 import sqlite3 as db
+import os
 from sqlite3 import OperationalError
 
 __author__ = 'Colin Tan'
 __version__ = 1.0
 
-fd = open('dbcreate.sql', 'r')
-sqlFile = fd.read()
-fd.close()
 
-sqlcmds = sqlFile.split(";")
+def main():
+    dir = os.path.dirname(__file__)
+    sqlfilename = os.path.join(dir, 'dbcreate.sql')
+    dbfilename = os.path.join(dir, 'database.db')
 
-conn = db.connect("database.db")
-cursor = conn.cursor()
+    fd = open(sqlfilename, 'r')
+    sqlFile = fd.read()
+    fd.close()
 
-for cmd in sqlcmds:
-    try:
-        cursor.execute(cmd)
-    except OperationalError, msg:
-        print("Command skipped: ", msg)
+    sqlcmds = sqlFile.split(";")
 
-conn.commit()
-cursor.close()
-conn.close()
-print("Finished creating the database")
+    conn = db.connect(dbfilename)
+    cursor = conn.cursor()
+
+    for cmd in sqlcmds:
+        try:
+            cursor.execute(cmd)
+        except OperationalError, msg:
+            print("Command skipped: ", msg)
+
+    conn.commit()
+    cursor.close()
+    conn.close()
+    return "Finished creating the database"
+
+if __name__ == "__main__":
+    main()
